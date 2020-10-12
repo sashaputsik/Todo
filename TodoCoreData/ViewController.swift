6e2cd8ee91a4d1a22cb9a41f6fecdaa09dab588a
alex <sashaputsikwork@gmail.com>
2020-10-12 13:24:07 +0300

    Initial Commit

---
+++ TodoCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoCoreData
+//
+//  Created by Sasha Putsikovich on 12.10.2020.
+//  Copyright © 2020 Sasha Putsikovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
